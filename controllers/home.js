06989bf9442e14028fe209c353eff298942d2629
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2021-03-11 19:59:11 +0200

    add route to create tasks

--- controllers/home.js
+++ controllers/home.js
@@ -2,3 +2,8 @@ exports.getPage = (req, res, next) => {
     //add passing of req.user
     res.render('home', {})
 }
+
+exports.createTask = (req, res, next) => {
+    console.log(req.body.task)
+    res.redirect('/')
+}
--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const home = require('../controllers/home')
 
 router.get('/', home.getPage);
+router.post('/add', home.createTask);
 
 module.exports = router
